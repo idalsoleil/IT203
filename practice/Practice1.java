bf8fba58aaa7783cf6e5ca80f156004b315ed6ec
idalsoleil <heyaldrick@gmail.com>
2024-08-20 20:08:11 +0800

    Update Practice1

--- practice/Practice1.java
+++ practice/Practice1.java
@@ -22,4 +22,47 @@ class Practice1 {
 
         scanner.close();
     }
+
+    /*
+    public static void getStrings(String[] cName) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Country: ");
+        cName[0] = sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        cName[1] = sc.nextLine();
+    }
+
+    public static void getDoubles(Double[] rate) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Rate: ");
+        rate[0] = sc.nextDouble();
+
+        
+    }
+
+    public static void getInt(int[] population) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Country's Population: ");
+        population[0] = sc.nextInt();
+    }
+
+    public static void main(String[] args) {
+
+        // Using array to pass arguments to method
+        String [] cName = new String[2]; 
+        Double [] rate = new Double[1];
+        int [] population = new int[1];
+
+        // Calling methods
+        getStrings(cName);
+        getDoubles(rate);
+        getInt(population);
+
+        // Output
+        System.out.println("Good morning " + cName[0] + "! It's so nice to be home again...\nIn this wonderful island with " + population[0] + " million people. And still growing at " + rate[0] + " percent per year.\nI will be staying here for good -- love " + cName[1]);
+    }
+    */
 }
